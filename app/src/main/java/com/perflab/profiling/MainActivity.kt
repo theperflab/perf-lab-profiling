6647290dcd85540e2b8219fb0e0ea511a9cc656c
theperflab <theperflab@gmail.com>
2024-02-04 16:12:51 +0530

    simple method tracing

--- app/src/main/java/com/perflab/profiling/MainActivity.kt
+++ app/src/main/java/com/perflab/profiling/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.perflab.profiling
 
 import android.os.Bundle
+import android.os.Debug
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+//        Generate Trace Logs
+//        -----------------------------
+
+//        1. Simple Tracing
+//        -----------------------------
+        Debug.startMethodTracing("sample-new")
         setContentView(R.layout.activity_main)
+        Debug.stopMethodTracing()
     }
 }
