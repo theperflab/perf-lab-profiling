8c0d926bfb39f8ad99c666875d34fc0dc73f5d82
theperflab <theperflab@gmail.com>
2024-02-04 16:14:38 +0530

    save multiple trace logs

--- app/src/main/java/com/perflab/profiling/MainActivity.kt
+++ app/src/main/java/com/perflab/profiling/MainActivity.kt
@@ -3,6 +3,9 @@ package com.perflab.profiling
 import android.os.Bundle
 import android.os.Debug
 import androidx.activity.ComponentActivity
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +17,15 @@ class MainActivity : ComponentActivity() {
 
 //        1. Simple Tracing
 //        -----------------------------
-        Debug.startMethodTracing("sample-new")
+//        Debug.startMethodTracing("sample-new")
+//        setContentView(R.layout.activity_main)
+//        Debug.stopMethodTracing()
+
+//        2. Save multiple trace logs
+//        -----------------------------
+        val dateFormat = SimpleDateFormat("dd_MM_yyyy_hh_mm_ss", Locale.getDefault())
+        val currentDateTime = dateFormat.format(Date())
+        Debug.startMethodTracing("sample-new-$currentDateTime")
         setContentView(R.layout.activity_main)
         Debug.stopMethodTracing()
     }
