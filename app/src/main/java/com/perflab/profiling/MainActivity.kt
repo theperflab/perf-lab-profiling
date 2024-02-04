2ad7ca040d3e34d5afb5f21301cc35f81ea8bad7
theperflab <theperflab@gmail.com>
2024-02-04 16:16:33 +0530

    advance tracing

--- app/src/main/java/com/perflab/profiling/MainActivity.kt
+++ app/src/main/java/com/perflab/profiling/MainActivity.kt
@@ -23,9 +23,15 @@ class MainActivity : ComponentActivity() {
 
 //        2. Save multiple trace logs
 //        -----------------------------
-        val dateFormat = SimpleDateFormat("dd_MM_yyyy_hh_mm_ss", Locale.getDefault())
-        val currentDateTime = dateFormat.format(Date())
-        Debug.startMethodTracing("sample-new-$currentDateTime")
+//        val dateFormat = SimpleDateFormat("dd_MM_yyyy_hh_mm_ss", Locale.getDefault())
+//        val currentDateTime = dateFormat.format(Date())
+//        Debug.startMethodTracing("sample-new-$currentDateTime")
+//        setContentView(R.layout.activity_main)
+//        Debug.stopMethodTracing()
+
+//        3. Advance Tracing
+//        -----------------------------
+        Debug.startMethodTracingSampling("sample-tracing-new", 1024 /*1KB*/ * 1024 /*1MB*/ * 8 /*8MB*/, 1000 /*1MS*/)
         setContentView(R.layout.activity_main)
         Debug.stopMethodTracing()
     }
